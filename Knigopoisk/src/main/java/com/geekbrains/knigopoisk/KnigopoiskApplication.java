2116329178940ae611e3735c838206fc386401ce
Stasrum <61759116+Stasrum@users.noreply.github.com>
2021-01-23 13:18:18 +0300

    Add project structure

---
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/KnigopoiskApplication.java
@@ -0,0 +1,13 @@
+package com.geekbrains.knigopoisk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnigopoiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnigopoiskApplication.class, args);
+	}
+
+}
