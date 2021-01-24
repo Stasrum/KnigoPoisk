2ba9725c7778978189ef44dbc1abf3f30ea5e4e0
Roman Zheludkov <romzhel@mail.ru>
2021-01-24 16:35:16 +0300

    - переименовал SystemUser в UserDto - добавил проверку сложности пароля

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/entities/UserDto.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/entities/UserDto.java
@@ -7,23 +7,24 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
 
 @Data
 @NoArgsConstructor
-@FieldMatchValidation(first = "password", second = "matchingPassword", message = "Пароль должен совпадать")
-public class SystemUser {
+@FieldMatchValidation(first = "password", second = "matchingPassword", message = "пароль должен совпадать")
+public class UserDto {
     @NotNull(message = "требуется")
     @Size(min = 3, message = "не менее 3 символов")
 //    @Pattern(regexp = "^[a-zA-Z0-9]{5}", message = "only 5 letters/digits")
     private String userName;
 
     @NotNull(message = "требуется")
-    @Size(min = 1, message = "требуется")
+    @Pattern(regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#&()–[{}]:;',?/*~$^+=<>]).{8,20}$",
+            message = "не менее 8 символов, включая заглавные и прописные буквы на латинице, цифры и спецсимволы")
     private String password;
 
     @NotNull(message = "требуется")
-    @Size(min = 1, message = "требуется")
     private String matchingPassword;
 
     @NotNull(message = "требуется")
@@ -36,10 +37,10 @@ public class SystemUser {
 
     @EmailValidation
     @NotNull(message = "требуется")
-    @Size(min = 6, message = "требуется")
     private String email;
 
     @BirthYearValidation
     @NotNull(message = "требуется")
-    private Integer birthYear;
+    @Size(min = 4, max = 4, message = "год в 4-х значном формате")
+    private String birthYear;
 }
