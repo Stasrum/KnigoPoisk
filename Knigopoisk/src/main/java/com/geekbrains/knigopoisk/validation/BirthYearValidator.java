dbcae8426f80744bdaebb662d2c5a7677d3e9523
Roman Zheludkov <romzhel@mail.ru>
2021-01-24 16:36:19 +0300

    - модифицировал валидатор даты рождения (исправил ошибку при нечисловом значении)

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/validation/BirthYearValidator.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/validation/BirthYearValidator.java
@@ -5,10 +5,15 @@ import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 import java.util.Calendar;
 
-public class BirthYearValidator implements ConstraintValidator<EmailValidation, Integer> {
+public class BirthYearValidator implements ConstraintValidator<BirthYearValidation, String> {
 
     @Override
-    public boolean isValid(final Integer birthYear, final ConstraintValidatorContext context) {
-        return birthYear > 1900 && birthYear <= Calendar.getInstance().get(Calendar.YEAR);
+    public boolean isValid(final String birthYear, final ConstraintValidatorContext context) {
+        try {
+            Integer year = Integer.parseInt(birthYear);
+            return year != null && year > 1900 && year <= Calendar.getInstance().get(Calendar.YEAR);
+        } catch (Exception e) {
+            return false;
+        }
     }
 }
