4c8546710f9197c5fa21ce04881467ab6aa1fdec
Roman Zheludkov <romzhel@mail.ru>
2021-02-05 00:00:57 +0300

    - вернул в соответствие с исходным проектом

--- Knigopoisk/src/main/resources/db/changelog/v-1.0/02-create-users-table.sql
+++ Knigopoisk/src/main/resources/db/changelog/v-1.0/02-create-users-table.sql
@@ -14,7 +14,9 @@ CREATE TABLE users(
                       created                 TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                       updated                 TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
                       PRIMARY KEY (id)
-) GO
+)
+
+GO
 
 INSERT INTO users (username, password, birth_day, email, enabled, account_non_expired, credentials_non_expired, account_non_locked, first_name , last_name) VALUES ('admin', '$2a$10$5uECjAbKQ86L.cW8naqBRu2dIue7LiaN9AWxys/B7vmoKHBJDLu7O','2020-01-02','admin@adm.co', true, true, true, true, 'alex','alex')
 
