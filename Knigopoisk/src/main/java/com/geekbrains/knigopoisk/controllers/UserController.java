073d5060287528888f98610128dca48f398f07c1
Roman Zheludkov <romzhel@mail.ru>
2021-02-05 19:21:32 +0300

    - контроллер упразднён, его содержимое перемещено в UserController

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/controllers/UserController.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/controllers/UserController.java
@@ -2,11 +2,17 @@ package com.geekbrains.knigopoisk.controllers;
 
 import com.geekbrains.knigopoisk.controllers.facade.UserControllerApi;
 import com.geekbrains.knigopoisk.entities.User;
+import com.geekbrains.knigopoisk.entities.UserDto;
+import com.geekbrains.knigopoisk.exceptions.UserAlreadyExistsException;
+import com.geekbrains.knigopoisk.exceptions.UserAttributeNotValidException;
 import com.geekbrains.knigopoisk.services.contracts.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
@@ -15,17 +21,33 @@ public class UserController implements UserControllerApi {
     private UserService userService;
 
     @Autowired
-    public void setUserService(UserService userService){
+    public void setUserService(UserService userService) {
         this.userService = userService;
     }
 
     @Override
-    public List<User> getAllUser(){
+    public List<User> getAllUser() {
         return userService.getAll();
     }
 
     @Override
-    public void deleteUserById(@PathVariable("id") @NotNull Long id){
+    public void deleteUserById(@PathVariable("id") @NotNull Long id) {
         userService.deleteByUserId(id);
+    }
+
+    // Binding Result после @ValidModel !!!
+    @Override
+    public void register(@Valid @RequestBody UserDto userDto, BindingResult theBindingResult) {
+        if (theBindingResult.hasErrors()) {
+            throw new UserAttributeNotValidException("Ошибка валидации", theBindingResult);
+        }
+
+        String userName = userDto.getUserName();
+        User existingUser = userService.findByUserName(userName);
+        if (existingUser != null) {
+            throw new UserAlreadyExistsException("Пользователь с таким именем уже существует");
+        }
+
+        userService.save(userDto);
     }
 }
--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/controllers/facade/UserControllerApi.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/controllers/facade/UserControllerApi.java
@@ -1,17 +1,25 @@
 package com.geekbrains.knigopoisk.controllers.facade;
 
 import com.geekbrains.knigopoisk.entities.User;
+import com.geekbrains.knigopoisk.entities.UserDto;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
+import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 import java.util.List;
 
 public interface UserControllerApi {
 
-    @GetMapping("/allusers")
+    @GetMapping("/users")
     List<User> getAllUser();
 
-    @GetMapping("/deluser/{id}")
+    @GetMapping("/users/delete/{id}")
     void deleteUserById(@PathVariable("id") @NotNull Long id);
+
+    @PostMapping("/users/register")
+    void register(@Valid @RequestBody UserDto userDto, BindingResult theBindingResult);
 }
