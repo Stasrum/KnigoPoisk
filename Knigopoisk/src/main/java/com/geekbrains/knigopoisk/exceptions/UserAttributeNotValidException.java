bb942ebd0881aeb3b3a6b0b9cd0538086b9fde13
Roman Zheludkov <romzhel@mail.ru>
2021-02-10 22:33:32 +0300

    - подправил обработку исключений

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/exceptions/UserAttributeNotValidException.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/exceptions/UserAttributeNotValidException.java
@@ -1,14 +1,14 @@
 package com.geekbrains.knigopoisk.exceptions;
 
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import org.springframework.validation.BindingResult;
 
+@EqualsAndHashCode(callSuper = true)
 @Data
-public class UserAttributeNotValidException extends RuntimeException {
-    private BindingResult bindingResult;
-
+public class UserAttributeNotValidException extends AttributeNotValidException {
     public UserAttributeNotValidException(String message, BindingResult bindingResult) {
-        super(message);
-        this.bindingResult = bindingResult;
+        super(message, bindingResult);
     }
 }
