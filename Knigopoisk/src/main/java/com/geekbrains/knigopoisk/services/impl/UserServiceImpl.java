9d120ddef7b6af5bc23cc76fb9f71bb45da78dfd
Roman Zheludkov <romzhel@mail.ru>
2021-02-10 22:34:05 +0300

    - добавил методы работы с ролями

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/impl/UserServiceImpl.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/impl/UserServiceImpl.java
@@ -9,9 +9,9 @@ import com.geekbrains.knigopoisk.entities.Role;
 import com.geekbrains.knigopoisk.entities.User;
 import com.geekbrains.knigopoisk.exceptions.UserNotFoundException;
 import com.geekbrains.knigopoisk.repositories.UserRepository;
+import com.geekbrains.knigopoisk.services.contracts.RoleService;
 import com.geekbrains.knigopoisk.services.contracts.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -20,6 +20,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -27,15 +28,10 @@ import java.util.stream.Collectors;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
-
-    @Autowired
-    private BCryptPasswordEncoder passwordEncoder;
-
-    @Autowired
-    private UserRepository userRepository;
-
-    @Autowired
-    private UserMapper userMapper;
+    private final BCryptPasswordEncoder passwordEncoder;
+    private final UserRepository userRepository;
+    private final RoleService roleService;
+    private final UserMapper userMapper;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
@@ -76,21 +72,20 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public boolean updateUserDetailsFromUserDetailsDto(UserDetailsDto userDetailsDto) {
         User user = userRepository.findById(userDetailsDto.getId()).orElseThrow(() ->
                 new UserNotFoundException("User id = <" + userDetailsDto.getId() + "> name = <" + userDetailsDto.getUserName() + "> not found"));
         userMapper.updateUserFromUserDetailsDto(userDetailsDto, user);
         userRepository.save(user);
-
         return true;
     }
 
     @Override
+    @Transactional
     public boolean updateUserPasswordFromUserPasswordDto(Long userId, UserPasswordDto userPasswordDto) {
-        User user = userRepository.findById(userId).orElseThrow(() ->
-                new UserNotFoundException("User id = <" + userId + "> not found"));
+        User user = getUserWithExistenceCheck(userId);
         user.setPassword(passwordEncoder.encode(userPasswordDto.getPassword()));
-
         return save(user);
     }
 
@@ -122,7 +117,48 @@ public class UserServiceImpl implements UserService {
     }
 
     @Override
+    @Transactional
     public boolean isUserByNameExists(String userName) {
         return userRepository.findUserByUsername(userName).isPresent();
+    }
+
+    @Override
+    @Transactional
+    public List<Role> getAssignedRolesByUserId(Long userId) {
+        User user = getUserWithExistenceCheck(userId);
+        return new ArrayList<>(user.getRoles());
+    }
+
+    @Override
+    @Transactional
+    public List<Role> getUnAssignedRolesByUserId(Long userId) {
+        User user = getUserWithExistenceCheck(userId);
+
+        return roleService.getAll().stream()
+                .filter(role -> user.getRoles().stream().noneMatch(userRole -> userRole.equals(role)))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    @Transactional
+    public List<Role> removeRoleByRoleName(Long userId, String roleName) {
+        User user = getUserWithExistenceCheck(userId);
+        Role role = roleService.getRoleByName(roleName);
+        user.getRoles().remove(role);
+        return new ArrayList<>(user.getRoles());
+    }
+
+    @Override
+    @Transactional
+    public List<Role> addRoleByRoleName(Long userId, String roleName) {
+        User user = getUserWithExistenceCheck(userId);
+        Role role = roleService.getRoleByName(roleName);
+        user.getRoles().add(role);
+        return new ArrayList<>(user.getRoles());
+    }
+
+    private User getUserWithExistenceCheck(Long userId) {
+        return userRepository.findById(userId).orElseThrow(() ->
+                new UserNotFoundException("User id = <" + userId + "> not found"));
     }
 }
