e3f1f5790e2eca81918fcd69303fab924286297e
Roman Zheludkov <romzhel@mail.ru>
2021-02-10 22:25:30 +0300

    - RoleRepository и RoleService (RoleServiceImpl)

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/contracts/RoleService.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/contracts/RoleService.java
@@ -2,6 +2,9 @@ package com.geekbrains.knigopoisk.services.contracts;
 
 import com.geekbrains.knigopoisk.entities.Role;
 
+import java.util.List;
+
 public interface RoleService {
     Role getRoleByName(String name);
+    List<Role> getAll();
 }
--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/impl/RoleServiceImpl.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/impl/RoleServiceImpl.java
@@ -1,19 +1,28 @@
 package com.geekbrains.knigopoisk.services.impl;
 
 import com.geekbrains.knigopoisk.entities.Role;
+import com.geekbrains.knigopoisk.exceptions.RoleNotFoundException;
 import com.geekbrains.knigopoisk.repositories.RoleRepository;
 import com.geekbrains.knigopoisk.services.contracts.RoleService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-@Service
-public class RoleServiceImpl implements RoleService {
+import java.util.List;
 
-    @Autowired
-    private RoleRepository roleRepository;
+@Service
+@RequiredArgsConstructor
+public class RoleServiceImpl implements RoleService {
+    private final RoleRepository roleRepository;
 
     @Override
-    public Role getRoleByName(String name){
-        return roleRepository.findOneByName(name);
+    public Role getRoleByName(String name) {
+        return roleRepository.findRoleByName(name).orElseThrow(() ->
+                new RoleNotFoundException("Role '" + name + "' not found"));
+    }
+
+    @Override
+    public List<Role> getAll() {
+        return roleRepository.findAll();
     }
 }
