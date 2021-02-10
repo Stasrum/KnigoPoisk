cfe6fb4bbaf2c0b35d1a612fbaf7a21efcc93a9c
Roman Zheludkov <romzhel@mail.ru>
2021-02-10 22:34:05 +0300

    - добавил методы работы с ролями

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/contracts/UserService.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/contracts/UserService.java
@@ -3,6 +3,7 @@ package com.geekbrains.knigopoisk.services.contracts;
 import com.geekbrains.knigopoisk.dto.UserDetailsDto;
 import com.geekbrains.knigopoisk.dto.UserPasswordDto;
 import com.geekbrains.knigopoisk.dto.UserRegistrationDto;
+import com.geekbrains.knigopoisk.entities.Role;
 import com.geekbrains.knigopoisk.entities.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.transaction.annotation.Transactional;
@@ -39,4 +40,16 @@ public interface UserService extends UserDetailsService {
 
     @Transactional
     boolean isUserByNameExists(String userName);
+
+    @Transactional
+    List<Role> getAssignedRolesByUserId(Long userId);
+
+    @Transactional
+    List<Role> getUnAssignedRolesByUserId(Long userId);
+
+    @Transactional
+    List<Role> removeRoleByRoleName(Long userId, String roleName);
+
+    @Transactional
+    List<Role> addRoleByRoleName(Long userId, String roleName);
 }
