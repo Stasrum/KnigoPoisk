9a81bfa6c262b2fc6529c5a6e41ed755fa58df4c
Roman Zheludkov <romzhel@mail.ru>
2021-02-10 23:24:03 +0300

    - откат на @Autowired для UserMapperDecorator

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/dto/mappers/UserMapperDecorator.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/dto/mappers/UserMapperDecorator.java
@@ -3,22 +3,23 @@ package com.geekbrains.knigopoisk.dto.mappers;
 import com.geekbrains.knigopoisk.dto.UserRegistrationDto;
 import com.geekbrains.knigopoisk.entities.User;
 import com.geekbrains.knigopoisk.services.contracts.RoleService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import java.util.Collections;
 
-@RequiredArgsConstructor
 public abstract class UserMapperDecorator implements UserMapper {
 
     @Autowired
     @Qualifier("delegate")
     private UserMapper delegate;
 
-    private final BCryptPasswordEncoder passwordEncoder;
-    private final RoleService roleService;
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Autowired
+    private RoleService roleService;
 
     @Override
     public User getUserFromUserRegistrationDto(UserRegistrationDto userRegistrationDto) {
