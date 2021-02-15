eebd1b585941516a50f3596c03690045725b4bca
indivioM <indivio@yandex.ru>
2021-02-15 20:03:31 +0300

    Change UserDto on UserForAdminsEditDto

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/dto/CommentDto.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/dto/CommentDto.java
@@ -2,7 +2,7 @@ package com.geekbrains.knigopoisk.dto;
 
 
 import com.geekbrains.knigopoisk.entities.Comment;
-import com.geekbrains.knigopoisk.entities.User;
+
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
@@ -11,19 +11,19 @@ import lombok.NoArgsConstructor;
 public class CommentDto {
     private Long id;
     private String text;
-    private User user;
+    private UserForAdminsEditDto user;
 
     public CommentDto(Comment comment) {
         this.id = comment.getId();
         this.text = comment.getText();
-        this.user = comment.getUser();
+        this.user = new UserForAdminsEditDto(comment.getUser());
     }
 
     public static Comment fromDto(CommentDto commentDto) {
         Comment comment = new Comment();
         comment.setId(commentDto.getId());
         comment.setText(commentDto.getText());
-        comment.setUser(commentDto.getUser());
+        comment.setUser(UserForAdminsEditDto.fromAdminsEditDto(commentDto.getUser()));
         return comment;
     }
 }
