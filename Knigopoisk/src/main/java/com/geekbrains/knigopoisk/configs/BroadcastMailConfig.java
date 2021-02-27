f853c9d8b6ee5edb5d23809f11b4e0b216ea8bc1
Valery Bulekov <muzaccord@gmail.com>
2021-02-27 16:04:43 +0300

    Настройки сервиса почтовой рассылки.

---
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/configs/BroadcastMailConfig.java
@@ -0,0 +1,44 @@
+package com.geekbrains.knigopoisk.configs;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Properties;
+
+@Configuration
+public class BroadcastMailConfig {
+
+    @Value("${mail.broadcast.host}")
+    private String host;
+
+    @Value("${mail.broadcast.port}")
+    private int port;
+
+    @Value("${mail.username}")
+    private String username;
+
+    @Value("${mail.password}")
+    private String password;
+
+    @Bean
+    public JavaMailSender javaMailSender() {
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(host);
+        mailSender.setPort(port);
+
+        mailSender.setUsername(username);
+        mailSender.setPassword(password);
+
+        Properties props = mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", "smtp");
+        props.put("mail.smtp.auth", "true");
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.debug", "true");
+        props.put("mail.from", "vbgeekbrains@gmail.com");
+
+        return mailSender;
+    }
+}
