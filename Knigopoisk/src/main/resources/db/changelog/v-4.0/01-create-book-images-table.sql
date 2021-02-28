923df86af55fd4e9fdd9c4f795c0a21cfcd4131d
Roman Zheludkov <romzhel@mail.ru>
2021-02-28 19:40:40 +0300

    - коррекция инициализации таблицы с BookImage

--- Knigopoisk/src/main/resources/db/changelog/v-4.0/01-create-book-images-table.sql
+++ Knigopoisk/src/main/resources/db/changelog/v-4.0/01-create-book-images-table.sql
@@ -1,5 +1,5 @@
 CREATE TABLE book_images (
-    id bigint,
+    id BIGSERIAL,
     book_id bigint not null,
     path varchar (255) NOT NULL,
     created TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
@@ -16,9 +16,9 @@ alter table if exists book_images
 
 GO
 
-INSERT INTO book_images VALUES (1, 1, '0af1f055-b221-4927-911c-1b0631f8dfa3tv1.jpg'),
-    (2, 2, '5dbd7dab-3f8f-4be3-86f5-a3b3ec9b534btv4.jpg'),
-    (3, 3, '6fe242d7-e633-4405-bdef-eea6da53f6detv6.jpg'),
-    (4, 4, '7b284ca2-ab86-49d7-85a7-4b415b15074ftv1.jpg')
+INSERT INTO book_images (book_id, path) VALUES (1, '0af1f055-b221-4927-911c-1b0631f8dfa3tv1.jpg'),
+    (2, '5dbd7dab-3f8f-4be3-86f5-a3b3ec9b534btv4.jpg'),
+    (3, '6fe242d7-e633-4405-bdef-eea6da53f6detv6.jpg'),
+    (4, '7b284ca2-ab86-49d7-85a7-4b415b15074ftv1.jpg')
 
 GO
