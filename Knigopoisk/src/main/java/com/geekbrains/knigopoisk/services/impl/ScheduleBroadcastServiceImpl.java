32271528cba4611c412449f2533f40953ca64256
Valery Bulekov <muzaccord@gmail.com>
2021-03-01 07:24:50 +0300

    Рассылка всем пользователем всех книг.

--- Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/impl/ScheduleBroadcastServiceImpl.java
+++ Knigopoisk/src/main/java/com/geekbrains/knigopoisk/services/impl/ScheduleBroadcastServiceImpl.java
@@ -1,12 +1,28 @@
 package com.geekbrains.knigopoisk.services.impl;
 
+import com.geekbrains.knigopoisk.dto.BookDto;
+import com.geekbrains.knigopoisk.dto.UserDetailsDto;
+import com.geekbrains.knigopoisk.entities.Book;
+import com.geekbrains.knigopoisk.entities.User;
+import com.geekbrains.knigopoisk.services.contracts.BookService;
+import com.geekbrains.knigopoisk.services.contracts.MailService;
 import com.geekbrains.knigopoisk.services.contracts.ScheduleBroadcastService;
+import com.geekbrains.knigopoisk.services.contracts.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Service
+@RequiredArgsConstructor
 public class ScheduleBroadcastServiceImpl implements ScheduleBroadcastService {
+
+    private final MailService mailService;
+    private final BookService bookService;
+    private final UserService userService;
 
     @Value("${broadcast.schedule.enable}")
     boolean broadcastEnable;
@@ -15,6 +31,17 @@ public class ScheduleBroadcastServiceImpl implements ScheduleBroadcastService {
     @Scheduled(cron = "${broadcast.schedule.time}")
     public void sendScheduleBroadcastMail() {
         if (broadcastEnable)
-            System.out.println("It works");
+            sendBroadcastMail();
+    }
+
+    private void sendBroadcastMail(){
+        List<User> users = userService.getAll().stream()
+                .map(UserDetailsDto::fromDto)
+                .collect(Collectors.toList());
+        List<Book> books = bookService.getAll().stream()
+                .map(BookDto::fromDto)
+                .collect(Collectors.toList());
+
+        mailService.sendBroadcastMail(users, books);
     }
 }
